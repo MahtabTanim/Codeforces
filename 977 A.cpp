f5a00ddfb6fa6d13851e96c48ab6fd253a31c4a1
MahtabTanim <63159321+MahtabTanim@users.noreply.github.com>
2020-04-28 22:58:58 +0600

    Add files via upload

---
+++ 977 A.cpp
@@ -0,0 +1,24 @@
+// Written By Mahtab Tanim
+#include<bits/stdc++.h>
+using namespace std;
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define End return 0
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define ull unsigned long long int
+#define pi acos(-1)
+#define forn(i,a,b) for (int i=a;i<=b;i++)
+int main()
+{
+int n,k;
+cin>>n>>k;
+while(k--)
+{
+    if(n%10==0) n=n/10;
+    else n=n-1;
+}
+cout<<n<<endl;
+End;
+}
