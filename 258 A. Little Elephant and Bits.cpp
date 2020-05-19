9157ad28e087ae04fee51eb74722adb80de6f643
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-05-19 16:21:50 +0600

    Update 258 A. Little Elephant and Bits.cpp

--- 258 A. Little Elephant and Bits.cpp
+++ 258 A. Little Elephant and Bits.cpp
@@ -14,18 +14,10 @@ int main()
 		if(s[i]=='0') zero++;
 	}
 
-	if(zero == s.length()) res="0";
-
-	else if(zero==0) 
+	if(zero==0) 
 	{
 		for(int i=0;i<s.length()-1;i++) res.push_back(s[i]);	
 	}
-	
-	else if(zero==1)
-	{
-		for(int i=0;i<s.length();i++) if(s[i]!='0') res.push_back(s[i]);		
-	}
-	
 	else
 	{
 		int count=0;
