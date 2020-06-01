01819345e704a8aea8572231d49e935ab8917bab
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-06-01 08:12:08 +0600

    Create 372 B. Hungry Sequence.cpp

---
+++ 372 B. Hungry Sequence.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<iterator>
+#include<algorithm>
+#include<map>
+#include<fstream>
+using namespace std;
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+fstream ff;
+
+long long  t,n,m,a,x,q,b,l,r,i,j,k,tcase=0;
+
+string s;
+
+int main()
+{
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cout<<100000+i<<endl;
+    }
+}
