a0a7f897d450711e633a7f7703f723da7e35b506
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-06-02 00:03:40 +0600

    Create 110 B. Lucky String.cpp

---
+++ 110 B. Lucky String.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include<iterator>
+#include<algorithm>
+#include<map>
+#include<list>
+#include<math.h>
+#include<fstream>
+#define pb push_back
+using namespace std;
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+long long  p=0,np=0,t,n,m,a,x,q,b,l,r,i,j,k,tcase=0;
+string s;
+
+int main()
+{
+    string s="abcd";
+    cin>>n;
+    for(int i=0;i<n;i++) cout<<s[i%4];
+    cout<<"\n";
+return 0;
+}
