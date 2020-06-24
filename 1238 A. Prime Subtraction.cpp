1f2de6a7800ba48e7f589ab5779054f4258b73b9
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2020-06-25 03:57:01 +0600

    Create 1238 A. Prime Subtraction.cpp

---
+++ 1238 A. Prime Subtraction.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long int 
+#define maxn 1000000
+#define FP ff.open("output.txt")
+fstream ff;    
+
+ll a,b,c,d,k,e,f,t,m,n,i,j,x,y,count,tcase=0;
+string s;
+
+int main()
+{
+	cin>>t;
+	while(t--)
+	{
+		cin>>n>>m;
+		if(n-m==1) cout<<"NO\n";
+		else cout<<"YES\n";
+	}
+  return 0;
+}
